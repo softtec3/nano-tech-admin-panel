86cdb8a669992401fd4d269f73b1e697dbd15f88
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2025-10-20 12:20:28 +0600

    ware house route all func done

--- src/pages/CreateWarehouse/CreateWarehouse.jsx
+++ src/pages/CreateWarehouse/CreateWarehouse.jsx
@@ -1,271 +1,309 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./createWarehouse.css";
 import { FaXmark } from "react-icons/fa6";
-// these should come from database
-const allWarehouses = [
-  {
-    warehouseName: "warehouse01",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-1",
-    productName: "Rice Cooker",
-    productQuantity: 50,
-  },
-  {
-    warehouseName: "warehouse01",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-1",
-    productName: "Blender",
-    productQuantity: 30,
-  },
-  {
-    warehouseName: "warehouse01",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-1",
-    productName: "Electric Kettle",
-    productQuantity: 70,
-  },
-  {
-    warehouseName: "warehouse02",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-2",
-    productName: "Microwave Oven",
-    productQuantity: 15,
-  },
-  {
-    warehouseName: "warehouse02",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-2",
-    productName: "Gas Stove",
-    productQuantity: 25,
-  },
-  {
-    warehouseName: "warehouse02",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-2",
-    productName: "LED TV",
-    productQuantity: 12,
-  },
-  {
-    warehouseName: "warehouse03",
-    warehouseSection: "Section-C",
-    warehouseSubSection: "Sub-3",
-    productName: "Refrigerator",
-    productQuantity: 8,
-  },
-  {
-    warehouseName: "warehouse03",
-    warehouseSection: "Section-C",
-    warehouseSubSection: "Sub-3",
-    productName: "Air Conditioner",
-    productQuantity: 18,
-  },
-  {
-    warehouseName: "warehouse03",
-    warehouseSection: "Section-C",
-    warehouseSubSection: "Sub-3",
-    productName: "Washing Machine",
-    productQuantity: 10,
-  },
-  {
-    warehouseName: "warehouse04",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-2",
-    productName: "Ceiling Fan",
-    productQuantity: 40,
-  },
-  {
-    warehouseName: "warehouse04",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-2",
-    productName: "Table Fan",
-    productQuantity: 25,
-  },
-  {
-    warehouseName: "warehouse04",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-2",
-    productName: "Exhaust Fan",
-    productQuantity: 15,
-  },
-  {
-    warehouseName: "warehouse05",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-1",
-    productName: "Laptop",
-    productQuantity: 20,
-  },
-  {
-    warehouseName: "warehouse05",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-1",
-    productName: "Desktop PC",
-    productQuantity: 12,
-  },
-  {
-    warehouseName: "warehouse05",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-1",
-    productName: "Printer",
-    productQuantity: 18,
-  },
-  {
-    warehouseName: "warehouse01",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-1",
-    productName: "Rice Cooker",
-    productQuantity: 50,
-  },
-  {
-    warehouseName: "warehouse01",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-1",
-    productName: "Blender",
-    productQuantity: 30,
-  },
-  {
-    warehouseName: "warehouse01",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-1",
-    productName: "Electric Kettle",
-    productQuantity: 70,
-  },
-  {
-    warehouseName: "warehouse02",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-2",
-    productName: "Microwave Oven",
-    productQuantity: 15,
-  },
-  {
-    warehouseName: "warehouse02",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-2",
-    productName: "Gas Stove",
-    productQuantity: 25,
-  },
-  {
-    warehouseName: "warehouse02",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-2",
-    productName: "LED TV",
-    productQuantity: 12,
-  },
-  {
-    warehouseName: "warehouse03",
-    warehouseSection: "Section-C",
-    warehouseSubSection: "Sub-3",
-    productName: "Refrigerator",
-    productQuantity: 8,
-  },
-  {
-    warehouseName: "warehouse03",
-    warehouseSection: "Section-C",
-    warehouseSubSection: "Sub-3",
-    productName: "Air Conditioner",
-    productQuantity: 18,
-  },
-  {
-    warehouseName: "warehouse03",
-    warehouseSection: "Section-C",
-    warehouseSubSection: "Sub-3",
-    productName: "Washing Machine",
-    productQuantity: 10,
-  },
-  {
-    warehouseName: "warehouse04",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-2",
-    productName: "Ceiling Fan",
-    productQuantity: 40,
-  },
-  {
-    warehouseName: "warehouse04",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-2",
-    productName: "Table Fan",
-    productQuantity: 25,
-  },
-  {
-    warehouseName: "warehouse04",
-    warehouseSection: "Section-A",
-    warehouseSubSection: "Sub-2",
-    productName: "Exhaust Fan",
-    productQuantity: 15,
-  },
-  {
-    warehouseName: "warehouse05",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-1",
-    productName: "Laptop",
-    productQuantity: 20,
-  },
-  {
-    warehouseName: "warehouse05",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-1",
-    productName: "Desktop PC",
-    productQuantity: 12,
-  },
-  {
-    warehouseName: "warehouse05",
-    warehouseSection: "Section-B",
-    warehouseSubSection: "Sub-1",
-    productName: "Printer",
-    productQuantity: 18,
-  },
-];
+import { getFormData } from "../../utils/getFormData";
+import { toast } from "react-toastify";
 
 const CreateWarehouse = () => {
-  const [warehouses, setWarehouses] = useState(allWarehouses);
   const [isShow, setIsShow] = useState(false);
-  const handleWarehouseData = (e) => {
-    const warehouseName = e.target.value;
-    setWarehouses(
-      allWarehouses.filter(
-        (warehouse) => warehouse.warehouseName === warehouseName
-      )
-    );
+  const [isShow2, setIsShow2] = useState(false);
+  const [isShow3, setIsShow3] = useState(false);
+  const [allWarehouses, setAllWarehouses] = useState([]);
+  const [allSections, setAllSections] = useState([]);
+  // Create warehouse
+  const handleCreateWarehouse = (e) => {
+    e.preventDefault();
+    const formData = getFormData(e.target);
+    if (formData?.warehouse_name == "" || formData?.warehouse_location == "") {
+      toast.error("All fields are required");
+      return;
+    }
+    try {
+      fetch(`${import.meta.env.VITE_API}/create_warehouse.php`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.success) {
+            toast.success(data?.message);
+          } else {
+            console.log(data?.message);
+          }
+        });
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      e.target.reset();
+      setIsShow(false);
+    }
   };
+  // get all warehouses
+  const getAllWarehouses = () => {
+    try {
+      fetch(`${import.meta.env.VITE_API}/all_warehouses.php`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.success) {
+            setAllWarehouses(data?.data);
+          } else {
+            console.log(data?.message);
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // create warehouse section
+  const handleCreateSection = (e) => {
+    e.preventDefault();
+    const formData = getFormData(e.target);
+    console.log(formData);
+    try {
+      fetch(`${import.meta.env.VITE_API}/create_warehouse_section.php`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.success) {
+            toast.success(data?.message);
+          } else {
+            toast.error("Something went wrong. Please check console");
+            console.log(data?.message);
+          }
+        });
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      e.target.reset();
+      setIsShow2(false);
+    }
+  };
+  // get all sections based on warehouse id
+  const handleGetSections = (e) => {
+    const warehouse_id = e.target.value;
+    try {
+      fetch(
+        `${
+          import.meta.env.VITE_API
+        }/all_sections_by_id.php?warehouse_id=${warehouse_id}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.success) {
+            setAllSections(data?.data);
+          } else {
+            toast.error(data?.message);
+            setAllSections([]);
+            console.log(data?.message);
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // Create sub section
+  const handleCreateSubSection = (e) => {
+    e.preventDefault();
+    const formData = getFormData(e.target);
+    try {
+      fetch(`${import.meta.env.VITE_API}/create_warehouse_sub_section.php`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.success) {
+            toast.success(data?.message);
+          } else {
+            console.log(data?.message);
+          }
+        })
+        .catch((error) => console.log(error.message));
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setIsShow3(false);
+      e.target.reset();
+    }
+  };
+  useEffect(() => {
+    getAllWarehouses();
+  }, []);
   return (
     <section id="warehouse">
       {/* warehouses and their products */}
       {/* Warehouse form container */}
       <div
-        id="warehouseContainerModal"
+        className="warehouseContainerModal"
         style={{ display: `${isShow ? "flex" : "none"}` }}
       >
         <main>
-          <form>
+          <form onSubmit={handleCreateWarehouse}>
             <span onClick={() => setIsShow(false)} className="cross">
               <FaXmark size={20} />
             </span>
             <h3>Create Warehouse</h3>
-            <div class="formElement">
-              <label for="warehouseName">
+            <div className="formElement">
+              <label htmlFor="warehouse_name">
                 Warehouse Name <span style={{ color: "red" }}>*</span>
               </label>
               <input
                 required
-                type="email"
-                name="warehouseName"
+                type="text"
+                name="warehouse_name"
                 placeholder="Enter warehouse name"
               />
             </div>
 
-            <div class="formElement">
-              <label for="warehouseLocation">
+            <div className="formElement">
+              <label htmlFor="warehouse_location">
                 Warehouse location <span style={{ color: "red" }}>*</span>
               </label>
               <input
                 required
                 type="text"
-                name="warehouseLocation"
+                name="warehouse_location"
                 placeholder="Enter warehouse location"
               />
             </div>
 
-            <div class="formElement">
-              <button className="btn">Create Role</button>
+            <div className="formElement">
+              <button type="submit" className="btn">
+                Create Warehouse
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+      {/* Create section */}
+      <div
+        className="warehouseContainerModal"
+        style={{ display: `${isShow2 ? "flex" : "none"}` }}
+      >
+        <main>
+          <form onSubmit={handleCreateSection}>
+            <span onClick={() => setIsShow2(false)} className="cross">
+              <FaXmark size={20} />
+            </span>
+            <h3>Create Warehouse Section</h3>
+            <div className="formElement">
+              <label htmlFor="warehouseName">
+                Warehouse Name <span style={{ color: "red" }}>*</span>
+              </label>
+              <select name="warehouse_id" required>
+                {/* these will fetch from database */}
+                <option value="" style={{ display: "none" }}>
+                  Select warehouse
+                </option>
+                {allWarehouses &&
+                  allWarehouses?.length > 0 &&
+                  allWarehouses.map((warehouse) => (
+                    <option key={warehouse?.id} value={warehouse?.id}>
+                      {warehouse?.warehouse_name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            <div className="formElement">
+              <label htmlFor="section_name">
+                Section Name <span style={{ color: "red" }}>*</span>
+              </label>
+              <input
+                required
+                type="text"
+                name="section_name"
+                placeholder="Enter section name"
+              />
+            </div>
+
+            <div className="formElement">
+              <button type="submit" className="btn">
+                Create Section
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+
+      {/* Create sub section */}
+      <div
+        className="warehouseContainerModal"
+        style={{ display: `${isShow3 ? "flex" : "none"}` }}
+      >
+        <main>
+          <form onSubmit={handleCreateSubSection}>
+            <span onClick={() => setIsShow3(false)} className="cross">
+              <FaXmark size={20} />
+            </span>
+            <h3>Create Sub Section</h3>
+            <div className="formElement">
+              <label htmlFor="warehouse_id">
+                Warehouse Name <span style={{ color: "red" }}>*</span>
+              </label>
+              <select onChange={handleGetSections} name="warehouse_id" required>
+                {/* these will fetch from database */}
+                <option value="" style={{ display: "none" }}>
+                  Select warehouse
+                </option>
+                {allWarehouses &&
+                  allWarehouses?.length > 0 &&
+                  allWarehouses.map((warehouse) => (
+                    <option key={warehouse?.id} value={warehouse?.id}>
+                      {warehouse?.warehouse_name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            <div className="formElement">
+              <label htmlFor="section_id">
+                Warehouse Section <span style={{ color: "red" }}>*</span>
+              </label>
+              <select
+                name="section_id"
+                required
+                disabled={allSections.length == 0 && true}
+              >
+                {/* these will fetch from database */}
+                <option value="" style={{ display: "none" }}>
+                  Select section
+                </option>
+                {allSections &&
+                  allSections?.length > 0 &&
+                  allSections.map((section) => (
+                    <option key={section?.id} value={section?.id}>
+                      {section?.section_name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+
+            <div className="formElement">
+              <label htmlFor="sub_section_name">
+                Warehouse Sub Section <span style={{ color: "red" }}>*</span>
+              </label>
+              <input
+                required
+                type="text"
+                name="sub_section_name"
+                placeholder="Enter sub section location"
+                disabled={allSections.length == 0 && true}
+              />
+            </div>
+
+            <div className="formElement">
+              <button className="btn">Create Sub Section</button>
             </div>
           </form>
         </main>
@@ -273,27 +311,48 @@ const CreateWarehouse = () => {
       <div className="warehouseContainer">
         <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
           <h2>All Warehouses</h2>
-          <select onChange={handleWarehouseData} name="warehouse">
+          <select name="warehouse">
             {/* these will fetch from database */}
             <option value="" style={{ display: "none" }}>
               All warehouses
             </option>
-            <option value="warehouse01">Warehouse 01</option>
-            <option value="warehouse02">Warehouse 02</option>
-            <option value="warehouse03">Warehouse 03</option>
-            <option value="warehouse04">Warehouse 04</option>
-            <option value="warehouse05">Warehouse 05</option>
+            {allWarehouses &&
+              allWarehouses?.length > 0 &&
+              allWarehouses.map((warehouse) => (
+                <option key={warehouse?.id} value={warehouse?.id}>
+                  {warehouse?.warehouse_name}
+                </option>
+              ))}
           </select>
         </div>
-        <button
-          onClick={() => {
-            setIsShow(true);
-          }}
-          className="btn"
-        >
-          Create Warehouse
-        </button>
+        <div className="warButtons">
+          <button
+            onClick={() => {
+              setIsShow(true);
+            }}
+            className="btn"
+          >
+            Create Warehouse
+          </button>
+          <button
+            onClick={() => {
+              setIsShow2(true);
+            }}
+            className="btn"
+          >
+            Create Section
+          </button>
+          <button
+            onClick={() => {
+              setIsShow3(true);
+            }}
+            className="btn"
+          >
+            Create Sub Section
+          </button>
+        </div>
       </div>
+      {/* all warehouse product will here */}
       {/* table */}
       <div id="salesPointTableContainer">
         <table>
@@ -306,17 +365,7 @@ const CreateWarehouse = () => {
               <th>Quantity</th>
             </tr>
           </thead>
-          <tbody>
-            {warehouses?.map((details, index) => (
-              <tr id="index">
-                <td>{details?.warehouseName}</td>
-                <td>{details?.warehouseSection}</td>
-                <td>{details?.warehouseSubSection}</td>
-                <td>{details?.productName}</td>
-                <td>{details?.productQuantity}</td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody></tbody>
         </table>
       </div>
     </section>
