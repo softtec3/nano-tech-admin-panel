7b9edd15b0b5f744a7b783e26abbb7e6e7d43649
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2025-11-03 10:07:55 +0600

    sales point report done

--- src/pages/SalesReport/SalesReport.jsx
+++ src/pages/SalesReport/SalesReport.jsx
@@ -187,25 +187,28 @@ const salesData = [
 const SalesReport = () => {
   // handle search functionality
   const [searchQuery, setSearchQuery] = useState("");
-  const [salesPoint, setSalesPoint] = useState(salesData);
+  const [salesPoint, setSalesPoint] = useState([]);
+  const [staticSalesPoint, setStaticSalesPoint] = useState([]);
   const [shopData, setShopData] = useState({ shopName: "", shopLocation: "" });
   const [totalSales, setTotalSales] = useState(0);
   const handleSearch = (e) => {
     e.preventDefault();
-    setSalesPoint(salesData.filter((data) => data.sellerId === searchQuery));
+    console.log(searchQuery);
+    const filteredProducts = staticSalesPoint.filter(
+      (point) => point.sales_point_id == searchQuery
+    );
+    setSalesPoint(filteredProducts);
   };
   useEffect(() => {
-    salesPoint.forEach((data) =>
-      setShopData({ shopName: data.shopName, shopLocation: data.shopLocation })
-    );
+    // salesPoint.forEach((data) =>
+    //   setShopData({ shopName: data.shopName, shopLocation: data.shopLocation })
+    // );
     const totalSalesPrice = salesPoint.reduce((sum, item) => {
       console.log(item);
-      return sum + item.soldPrice;
+      return sum + item.payable_amount;
     }, 0);
     setTotalSales(totalSalesPrice);
   }, [salesPoint]);
-  console.log(salesPoint);
-  console.log(shopData);
   // filter
   const handleFilter = (e) => {
     const value = e.target.value;
@@ -223,6 +226,26 @@ const SalesReport = () => {
         console.log("all");
     }
   };
+  // fetching sales points order
+  useEffect(() => {
+    try {
+      fetch(`${import.meta.env.VITE_API}/all_sales_points_orders.php`, {
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.success) {
+            setSalesPoint(data?.data);
+            setStaticSalesPoint(data?.data);
+          } else {
+            console.log(data?.message);
+          }
+        });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+  console.log(salesPoint);
   return (
     // sales point section
     <section id="salesPoint">
@@ -236,12 +259,21 @@ const SalesReport = () => {
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 type="text"
-                placeholder="Enter Shop ID"
+                placeholder="Enter sales point id"
               />
               <button type="submit">Search</button>
+              <button
+                onClick={() => {
+                  setSalesPoint(staticSalesPoint);
+                }}
+                type="button"
+                style={{ backgroundColor: "red", marginLeft: "5px" }}
+              >
+                Reset
+              </button>
             </form>
           </div>
-          {shopData && (
+          {/* {shopData && (
             <div className="flex" style={{ gap: "10px" }}>
               <div>
                 <b>Shop: </b>
@@ -252,11 +284,11 @@ const SalesReport = () => {
                 {shopData.shopLocation}
               </div>
             </div>
-          )}
+          )} */}
         </div>
         {/* sales points report table */}
         <div className="salesPointSummary">
-          <h4>Total Sales: {totalSales}TK</h4>
+          <h4>Total Sales: {totalSales} TK</h4>
           <select onChange={handleFilter} name="filter">
             <option value="all">Filter (All)</option>
             <option value="this_week">This week</option>
@@ -268,25 +300,35 @@ const SalesReport = () => {
           <table>
             <thead>
               <tr>
-                <th>Seller ID</th>
-                <th>Shop Name</th>
-                <th>Sold Date</th>
+                <th>SP ID</th>
+                <th>SP Name</th>
+                <th>Product ID</th>
                 <th>Product Name</th>
                 <th>Product Price</th>
+                <th>Discount</th>
                 <th>Sold Price</th>
+                <th>Payment Type</th>
+                <th>Sold Date</th>
               </tr>
             </thead>
             <tbody>
-              {salesPoint?.map((point, index) => (
-                <tr key={index}>
-                  <td>{point?.sellerId}</td>
-                  <td>{point?.shopName}</td>
-                  <td>{point?.soldDate}</td>
-                  <td>{point?.productName}</td>
-                  <td>{point?.productPrice}TK</td>
-                  <td>{point?.soldPrice}Tk</td>
-                </tr>
-              ))}
+              {salesPoint &&
+                salesPoint.length > 0 &&
+                salesPoint?.map((point) => (
+                  <tr key={point?.id}>
+                    <td>{point?.sales_point_id}</td>
+                    <td>{point?.sales_point_name}</td>
+                    <td>{point?.product_id}</td>
+                    <td>{point?.product_name}</td>
+                    <td>{point?.price} TK</td>
+                    <td>{point?.discount_amount} TK</td>
+                    <td>{point?.payable_amount} TK</td>
+                    <td style={{ textTransform: "capitalize" }}>
+                      {point?.payment_type}
+                    </td>
+                    <td>{point?.created_at}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
